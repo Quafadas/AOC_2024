48fdbfcffc7b56085c47e27f98d7b8083aa2a37c
partens <simon.parten@schroders.com>
2024-12-09 17:38:04 +0100

    .

--- 9/day9.scala
+++ 9/day9.scala
@@ -2,19 +2,90 @@ import pprint.*
 
 import scala.Long
 
+enum Direction:
+  case Head, Tail
+
 enum DiskBlock:
   case FakeFile(id: Long, length: Long) extends DiskBlock
   case Space(length: Long) extends DiskBlock
+  case PartialSpace extends DiskBlock
+  case PartialFile(parent: FakeFile) extends DiskBlock
+  def blockLength =
+    this match
+      case FakeFile(_, length) => length
+      case Space(length)       => length
+      case PartialSpace        => 1L
+      case PartialFile(parent) => 1L
 
 @main def day9 =
   val input = readTestInputForDay(9).mkString
-  val makeBlocky = input.foldLeft[(List[DiskBlock], DiskBlock, Long)](List.empty[DiskBlock], DiskBlock.Space(0L), 0L) { case ((acc, prior, priorId), char) =>
+  val (makeBlocky, _, _) = input.foldLeft[(List[DiskBlock], DiskBlock, Long)](List.empty[DiskBlock], DiskBlock.Space(0L), 0L) { case ((acc, prior, priorId), char) =>
     val (newBLock, newId) = prior match
       case DiskBlock.Space(_) =>
-        println(char)
+        // println(char)
         (DiskBlock.FakeFile(1L + priorId, char.asDigit.toLong), 1L + priorId)
       case DiskBlock.FakeFile(_, _) =>
-        (DiskBlock.Space(char.toLong), priorId)
+        (DiskBlock.Space(char.asDigit.toLong), priorId)
+      case _ => ???
     (acc :+ newBLock, newBLock, newId)
   }
+
+  val diskSize = makeBlocky.foldLeft(0L) { case (acc, block) =>
+    acc + block.blockLength
+  }
+
+  def reorder(headIdx: Int, tailIdx: Int, diskBlocks: List[DiskBlock], checksum: Long, direction: Direction): (Long, List[DiskBlock]) =
+    pprintln(s"headIdx: $headIdx, tailIdx: $tailIdx, checksum: $checksum, direction: $direction")
+    pprintln(diskBlocks)
+    if (diskBlocks.isEmpty)
+      (checksum, diskBlocks)
+    else {
+      // val (head_headSide, tail_headSide) = diskBlocks.splitAt(headIdx)
+      val (head_tailSide, tail_tailSide) = diskBlocks.splitAt(tailIdx)
+
+      direction match
+        case Direction.Head =>
+          diskBlocks.head match
+            case DiskBlock.FakeFile(id, length) =>
+              // Increment the headidx by 1, calcualte the checksum increment forwards
+              val newHeadIdx = headIdx + length.toInt
+              val newChecksum = checksum + ((headIdx until headIdx + length.toInt).sum * id).toLong
+              reorder(newHeadIdx, tailIdx, diskBlocks.tail, newChecksum, Direction.Head)
+            case DiskBlock.Space(length) =>
+              val expandSpces = List.fill(length.toInt)(DiskBlock.PartialSpace) ++ diskBlocks.tail
+              reorder(headIdx, tailIdx, expandSpces, checksum, Direction.Tail)
+            case DiskBlock.PartialSpace =>
+              diskBlocks.last match
+                case DiskBlock.FakeFile(id, length) => reorder(headIdx, tailIdx, diskBlocks, checksum, Direction.Tail)
+                case DiskBlock.Space(length)        => reorder(headIdx, tailIdx, diskBlocks, checksum, Direction.Tail)
+                case DiskBlock.PartialSpace         => reorder(headIdx, tailIdx, diskBlocks, checksum, Direction.Tail)
+                case DiskBlock.PartialFile(parent) =>
+                  ???
+
+            case DiskBlock.PartialFile(parent) =>
+              val newHeadIdx = headIdx + 1
+              val newChecksum = checksum + parent.id * headIdx
+              reorder(newHeadIdx, tailIdx, diskBlocks, newChecksum, Direction.Head)
+
+        case Direction.Tail =>
+          diskBlocks.last match {
+            case d: DiskBlock.FakeFile =>
+              val expanded = diskBlocks.take(diskBlocks.length - 1) ++ List.fill(d.length.toInt)(DiskBlock.PartialFile(d))
+              reorder(headIdx, tailIdx, expanded, checksum, Direction.Head)
+            case DiskBlock.Space(length) =>
+              val dropTailSpace = diskBlocks.take(diskBlocks.length - 1)
+              reorder(headIdx, tailIdx, dropTailSpace, checksum, Direction.Tail)
+            case DiskBlock.PartialSpace =>
+              val dropTailSpace = diskBlocks.take(diskBlocks.length - 1)
+              reorder(headIdx, tailIdx, dropTailSpace, checksum, Direction.Tail)
+            case DiskBlock.PartialFile(parent) =>
+              reorder(headIdx, tailIdx, diskBlocks, checksum, Direction.Head)
+          }
+
+    }
+
   pprintln(makeBlocky)
+
+  val part1 = reorder(1, 1, makeBlocky, 0L, Direction.Head)
+
+  pprintln(part1)
--- project.scala
+++ project.scala
@@ -2,6 +2,6 @@
 
 //> using javaOpt --add-modules=jdk.incubator.vector
 
-//> using dep io.github.quafadas::vecxt::0.0.22-12-c0cb0e
+//> using dep io.github.quafadas::vecxt::0.0.23
 //> using dep com.lihaoyi::fastparse:3.1.1
 //> using dep com.lihaoyi::pprint::0.9.0
