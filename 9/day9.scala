0239aa5768362be853dc9aba148abfec1fffa619
Simon Parten <quafadas@gmail.com>
2024-12-09 08:58:46 +0100

    .

--- 9/day9.scala
+++ 9/day9.scala
@@ -1,2 +1,20 @@
+import pprint.*
+
+import scala.Long
+
+enum DiskBlock:
+  case FakeFile(id: Long, length: Long) extends DiskBlock
+  case Space(length: Long) extends DiskBlock
+
 @main def day9 =
-  println("Day 9")
+  val input = readTestInputForDay(9).mkString
+  val makeBlocky = input.foldLeft[(List[DiskBlock], DiskBlock, Long)](List.empty[DiskBlock], DiskBlock.Space(0L), 0L) { case ((acc, prior, priorId), char) =>
+    val (newBLock, newId) = prior match
+      case DiskBlock.Space(_) =>
+        println(char)
+        (DiskBlock.FakeFile(1L + priorId, char.asDigit.toLong), 1L + priorId)
+      case DiskBlock.FakeFile(_, _) =>
+        (DiskBlock.Space(char.toLong), priorId)
+    (acc :+ newBLock, newBLock, newId)
+  }
+  pprintln(makeBlocky)
